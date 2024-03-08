87f1d9e34e57f45b211ff187eaf297e2519cc0bc
Daniel Sedigh <seddigh.daniel@gmail.com>
2024-03-08 14:44:45 +0100

    cant seem to load the image.

--- ObjectDetection/Main.cpp
+++ ObjectDetection/Main.cpp
@@ -1,10 +1,17 @@
 #include "Login.h"
 #include "App.h"
+#include "MainView.h"
 wxIMPLEMENT_APP(App);
 bool App::OnInit() {
-	Login* login = new Login("Login");
+	/*Login* login = new Login("Login");
 	login->SetClientSize(300, 300);
 	login->Center();
-	login->Show();
+	login->Show();*/
+	MainView* main = new MainView("Main");
+	main->SetClientSize(800, 600);
+	main->Center();
+	main->Show();
 	return true;
+
+
 }
--- ObjectDetection/MainView.cpp
+++ ObjectDetection/MainView.cpp
@@ -1,5 +1,6 @@
 #include <wx/wx.h>
 #include "MainView.h"
+#include <iostream>
 // OpenFileDialog for choosing picture to process
 
 // NotificationMessage when a person enters wrong username or pwd
@@ -11,11 +12,16 @@
 // Others > FrameIcon for a custom icon
 
 MainView::MainView(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
-	wxImage img(wxT(), wxBITMAP_TYPE_ANY);
+	std::cout << "Starting frame" << std::endl;
+	wxImage img(wxT("IMG-20240211-WA0024.jpg"));
+	std::cout << "Managed to insert pic to wxImage" << std::endl;
 	if (img.IsOk()) {
+		std::cout << "Image seems ok" << std::endl;
 		wxBitmap bitmap(img);
+		std::cout << "Converted imaged to a wxBitmap" << std::endl;
 
 		wxStaticBitmap* static_bit = new wxStaticBitmap(this, wxID_ANY, bitmap);
+
 
 		wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 		sizer->Add(static_bit, 1, wxEXPAND | wxALL, 10);
