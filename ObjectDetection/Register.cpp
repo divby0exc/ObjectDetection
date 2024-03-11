a600c76d4ea8f54d6fc58bde0dca3691e5233995
Daniel Sedigh <seddigh.daniel@gmail.com>
2024-03-11 20:37:57 +0100

    Binding to register btn works

--- ObjectDetection/Register.cpp
+++ ObjectDetection/Register.cpp
@@ -4,6 +4,7 @@
 Register::Register(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 	wxButton* register_user = new wxButton(panel, wxID_ANY, "Register", wxPoint(10, 220), wxSize(100, 50));
+	register_user->Bind(wxEVT_BUTTON, &Register::register_user, this);
 
 	wxButton* abort_btn = new wxButton(panel, wxID_ANY, "Abort", wxPoint(190, 220), wxSize(100, 50));
 	abort_btn->Bind(wxEVT_BUTTON, &Register::clear_fields, this);
@@ -24,4 +25,5 @@ void Register::clear_fields(wxCommandEvent& evt)
 void Register::register_user(wxCommandEvent& evt)
 {
 	// POCO
+	wxMessageBox("Working");
 }
